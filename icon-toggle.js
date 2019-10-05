a444a9fb8c44c8b64c1714ec39177659aef4f308
Alencruser <alencruserxx@hotmail.fr>
2019-10-05 16:26:05 +0200

    initial commit

--- icon-toggle.js
+++ icon-toggle.js
@@ -4,18 +4,22 @@ import '@polymer/iron-icon/iron-icon.js';
 class IconToggle extends PolymerElement {
   static get template() {
     return html`
-      <style>
-        /* shadow DOM styles go here */
-        span {
-          color: blue;
-        }
-        :host {
-          display: inline-block;
-        }
-      </style>
+    <style>
+    :host {
+      display: inline-block;
+    }
+    iron-icon {
+      fill: rgba(0,0,0,0);
+      stroke: currentcolor;
+    }
+    :host([pressed]) iron-icon {
+      fill: currentcolor;
+    }
+  </style>
+  
   
       <!-- shadow DOM goes here -->
-      <span>Not much here yet.</span>
+      <iron-icon icon="polymer"></iron-icon>
     `;
   }
   constructor() {
